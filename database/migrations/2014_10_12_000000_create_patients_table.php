65e991cf967eea5beea68115da675318de442bf2
DallingtonAsin <asingwire50dallington@gmail.com>
2023-04-22 15:39:29 +0300

    added is_verified to doctors table

--- database/migrations/2014_10_12_000000_create_patients_table.php
+++ database/migrations/2014_10_12_000000_create_patients_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 class CreatePatientsTable extends Migration
 {
@@ -30,8 +32,8 @@ class CreatePatientsTable extends Migration
             $table->string('fcm_token')->nullable();
             $table->string('otp')->nullable();
             $table->string('image')->nullable();
-            $table->boolean('is_blocked')->default(false);
             $table->boolean('profile_status')->default(false);
+            $table->boolean('is_blocked')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
@@ -47,6 +49,8 @@ class CreatePatientsTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::dropIfExists('patients');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
