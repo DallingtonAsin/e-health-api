1ace11dbefb401dbf4dcca1a4da964ce341050cb
DallingtonAsin <asingwire50dallington@gmail.com>
2023-04-29 03:17:54 +0300

    refactored endpoint for fetching appointments

--- app/Repositories/MedicalAppointmentRepository.php
+++ app/Repositories/MedicalAppointmentRepository.php
@@ -72,7 +72,7 @@ class MedicalAppointmentRepository
         $appointments = $this->medicalAppointment->with(['patient' => function ($query) {
             $query->select(['id', 'first_name', 'last_name', 'country_code', 'phone_number', 'email', 'address', 'dob', 'image']);
         }])->with(['doctor' => function ($query) {
-            $query->select(['id', 'first_name', 'last_name', 'specialty_id', 'title', 'country_code', 'phone_number', 'email', 'qualification', 'profession', 'experience', 'image', 'service_fee']);
+            $query->select(['id', 'first_name', 'last_name', 'specialty_id', 'country_code', 'phone_number', 'email', 'qualification', 'image', 'service_fee']);
         }])->with(['appointmentType' => function ($query) {
             $query->select(['id', 'name']);
         }])->with(['meetingAccess' => function ($query) {
